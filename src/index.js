f762ea9cf94799f9e09ce9f4efd2af5a908c01ef
David Blanco <david.blanco@maeztra.com>
2022-01-13 21:17:17 -0300

    Types

--- src/index.js
+++ src/index.js
@@ -3,17 +3,21 @@ import { ApolloServer, gql } from 'apollo-server'
 const server = new ApolloServer({
     typeDefs: gql`
     type Query{
-        hello: String
-        hi : String
+        id: ID
+        name : String
+        age : Int
+        average : Float
+        maried:Boolean!
+        arrayString:[String]
     }`,
     resolvers: {
         Query: {
-            hello: () => {
-                return 'Hello again'
-            },
-            hi: () => {
-                return 'hi'
-            }
+            id: () => '349583905-dfdgidojoi-dfgidjgod',
+            name: () => 'name',
+            age: () => 30,
+            average: () => 50.55,
+            maried: () => false,//whei it takes ! in the types above resolver cant return null
+            arrayString: () => ['a', 'b']
         },
     }
 });
